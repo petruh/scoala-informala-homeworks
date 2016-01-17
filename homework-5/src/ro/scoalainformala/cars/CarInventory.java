7e63452d84b767ad309d5da26d040c5f025856c0
Petru Hangiu <petru.hangiu@gmail.com>
2016-01-17 23:04:44 +0200

    Fixed compilation problem caused by line 56 in CarInventory.java. (sort method is not implemeted for ArrayList in java 1.7). Added javaDoc.

--- homework-5/src/ro/scoalainformala/cars/CarInventory.java
+++ homework-5/src/ro/scoalainformala/cars/CarInventory.java
@@ -1,29 +1,59 @@
 package ro.scoalainformala.cars;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * <P>This class models a car inventory. 
+ * <P>The Car objects are held inside a HashMap. 
+ * 
+ * @author Petru
+ *
+ */
 public class CarInventory {
 	private HashMap<String, Car> map = new HashMap<String,Car>();
 	
+	/**
+	 * Adds a Car to the internal HashMap used for storage.
+	 * @param car the Car object to be added
+	 */
 	public void addCar(Car car) {
 		map.put(car.getChassisNumber(), car);
 	}
 	
+	/**
+	 * Retrieves a car from the internal HashMap based on its chassis number.
+	 * 
+	 * @param chassisNumber the key based on which the Car object is retrieved from the internal HashMap.
+	 * @return the car object inside the internal HashMap that has the provided <tt>chassis number</tt>. 
+	 */
 	public Car findCar(String chassisNumber){
 		return map.get(chassisNumber);
 	}
 	
+	/**
+	 * Removes a car from the internal HashMap based on its chassis number.
+	 * 
+	 * @param chassisNumber the key based on which the Car object is removed from the internal HashMap.
+	 */
 	public void removeCar(String chassisNumber) {
 		map.remove(chassisNumber);
 	}
 	
+	/**
+	 * This method returns a list with the most fuel efficient cars in the inventory.
+	 * <p>The Car objects from the internal HashMap are transfered to an ArrayList and then sorted based on their fuel consumption.
+	 * <p>The first Car in the sorted ArrayList is held as a reference for the efficiency. The list is then iterated through and all the Cars with an efficiency that is different from the reference are removed.
+	 * 
+	 * @return an <tt>ArrayList<Car>()</tt> with the most fuel efficient cars.
+	 */
 	public List<Car> getMostFuelEfficientCars(){
 		List<Car> carList = new ArrayList<Car>();
 		carList.addAll(map.values());			
-		carList.sort(new Comparator<Car>(){
+		Collections.sort(carList, new Comparator<Car>(){
 			@Override
 			public int compare(Car c1, Car c2) {
 				Float ef1 = c1.getFuelConsumption(5, 1);
--- homework-5/src/ro/scoalainformala/cars/CarWash.java
+++ homework-5/src/ro/scoalainformala/cars/CarWash.java
@@ -6,36 +6,69 @@ import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * <P>This class models a car wash. 
+ * <P>The Car objects are held inside a LinkedList that models the wash queue.
+ * <P>The class also keeps track of the clients that use the car wash.
+ * 
+ * @author Petru
+ *
+ */
 public class CarWash {
 	private LinkedList<Car> washQueue;
 	private LinkedHashSet<Car> todaysClients;
 	private ArrayList<Car> postponedClients;
 
+	/**
+	 *<em>Opens</em> the Car Wash.
+	 *New <i>Collections<i> are created for the internal fields that hold today's clients and the wash queue. 
+	 * 
+	 */
 	public void openShop() {
 		washQueue = new LinkedList<Car> ();
 		todaysClients = new LinkedHashSet<Car>();
 	}
 	
+	/**
+	 * <em>Closes</em> the Car Wash.
+	 * When the shop is closed all the clients that were waiting in the wash queue are transfered to the <em>postponed clients</em> ArrayList.
+	 * The washQueue is then emptied.
+	 */
 	public void closeShop() {
 		postponedClients = new ArrayList<Car>();
 		postponedClients.addAll(washQueue);
 		washQueue.removeAll(washQueue);
 	}
 	
+	/**
+	 * Adds a Car to the wash queue.
+	 * @param c The Car object to be added to the wash queue.
+	 */
 	public void standInLine(Car c) {
 		washQueue.add(c);
 	}
 	
+	/**
+	 * Removes the first Car from the wash queue and adds it to the HashSet that holds today's clients.
+	 */
 	public void washCar() {
 		todaysClients.add(washQueue.removeFirst());
 	}
 	
+	/**
+	 * Removes the car provided as a parameter from the wash queue and adds it to the HashSet that holds today's clients.
+	 * @param c the car to be washed
+	 */
 	public void washCar(Car c) {
 		if (washQueue.remove(c)) {
 			todaysClients.add(c);
 		}
 	}
 	
+	/**
+	 * Returns an <em>ArrayList</em> with all the clients that got their car washed before the shop closed.
+	 * @return A new <em>ArrayList</em> that is a copy of the internal class field <em>todaysClients</em>
+	 */
 	public List<Car> getTodaysClients() {
 		Car[] cars = new Car[todaysClients.size()];
 		cars = todaysClients.toArray(cars);
@@ -43,6 +76,10 @@ public class CarWash {
 		return todaysClients;
 	}
 	
+	/**
+	 * Returns an <em>ArrayList</em> with all the clients that were in the wash queue but didn't get their car washed before the shop closed.
+	 * @return An <em>ArrayList</em> with the postponed clients.
+	 */
 	public List<Car> getPostponedClients() {
 		return postponedClients;
 	}
